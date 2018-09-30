0fb6bb2a1a9155462e965c1082b21423bd28cf32
ALEJANDRA SABOGAL <ja.sabogal@uniandes.edu.co>
2018-09-29 21:44:52 -0500

    Paso 6: Se agregan pruebas para obtener el promedio de 0,1 o más elementos (Rojo)

--- test_numbers.py
+++ test_numbers.py
@@ -4,16 +4,16 @@ import numbers
 
 class TestNumbers(unittest.TestCase):
     def test_info_numbers_empty(self):
-        self.assertEqual(numbers.get_info_numbers(""), [0, 0, 0])
+        self.assertEqual(numbers.get_info_numbers(""), [0, 0, 0, 0])
 
     def test_info_numbers_one_element(self):
-        self.assertEqual(numbers.get_info_numbers("2"), [1, 2, 2])
+        self.assertEqual(numbers.get_info_numbers("2"), [1, 2, 2, 2])
 
     def test_info_numbers_two_elements(self):
-        self.assertEqual(numbers.get_info_numbers("2,5"), [2, 2, 5])
+        self.assertEqual(numbers.get_info_numbers("2,5"), [2, 2, 5, 3.5])
 
     def test_info_numbers_n_elements(self):
-        self.assertEqual(numbers.get_info_numbers("2,5,10,3,311"), [5, 2, 311])
+        self.assertEqual(numbers.get_info_numbers("2,5,10,3,311"), [5, 2, 311, 66.2])
 
 
 if __name__ == '__main__':
