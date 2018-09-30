56d1ca4b0df69bd14e1f66bf0c9516ae326f3a06
ALEJANDRA SABOGAL <ja.sabogal@uniandes.edu.co>
2018-09-29 20:51:38 -0500

    Paso 1: Se crea el test cuando el número de elementos es cero (Rojo)

--- test_numbers.py
+++ test_numbers.py
@@ -2,6 +2,9 @@ import unittest
 import numbers
 
 class TestNumbers(unittest.TestCase):
+    def test_amountNumbers_empty(self):
+        self.assertEqual(numbers.get_amount_numbers(""), [0])
+
 
 if __name__ == '__main__':
     unittest.main()
