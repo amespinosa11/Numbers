3857d466dd2e3fa4cde93886dc20aec07b9ed760
ALEJANDRA SABOGAL <ja.sabogal@uniandes.edu.co>
2018-09-29 21:26:46 -0500

    Paso 4: Se agregan las pruebas para arreglos con uno o más elementos para obtener la cantidad de elementos y el mínimo (Rojo)

--- test_numbers.py
+++ test_numbers.py
@@ -7,13 +7,13 @@ class TestNumbers(unittest.TestCase):
         self.assertEqual(numbers.get_info_numbers(""), [0, 0])
 
     def test_info_numbers_one_element(self):
-        self.assertEqual(numbers.get_info_numbers("2"), [1])
+        self.assertEqual(numbers.get_info_numbers("2"), [1, 2])
 
     def test_info_numbers_two_elements(self):
-        self.assertEqual(numbers.get_info_numbers("2,5"), [2])
+        self.assertEqual(numbers.get_info_numbers("2,5"), [2, 5])
 
     def test_info_numbers_n_elements(self):
-        self.assertEqual(numbers.get_info_numbers("2,5,10,3,311"), [5])
+        self.assertEqual(numbers.get_info_numbers("2,5,10,3,311"), [5, 2])
 
 
 if __name__ == '__main__':
