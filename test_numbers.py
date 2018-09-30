74d3266f0bb3fb2854640296baf34cba1a51dc48
ALEJANDRA SABOGAL <ja.sabogal@uniandes.edu.co>
2018-09-29 21:21:04 -0500

    Paso 3: Se unen pruebas con parámetros comunes esperando resultado de arreglo con dos elementos (Azul)

--- test_numbers.py
+++ test_numbers.py
@@ -3,20 +3,18 @@ import numbers
 
 
 class TestNumbers(unittest.TestCase):
-    def test_amountNumbers_empty(self):
-        self.assertEqual(numbers.get_amount_numbers(""), [0])
+    def test_info_numbers_empty(self):
+        self.assertEqual(numbers.get_info_numbers(""), [0, 0])
 
-    def test_amountNumbers_one_element(self):
-        self.assertEqual(numbers.get_amount_numbers("2"), [1])
+    def test_info_numbers_one_element(self):
+        self.assertEqual(numbers.get_info_numbers("2"), [1])
 
-    def test_amountNumbers_two_elements(self):
-        self.assertEqual(numbers.get_amount_numbers("2,5"), [2])
+    def test_info_numbers_two_elements(self):
+        self.assertEqual(numbers.get_info_numbers("2,5"), [2])
 
-    def test_amountNumbers_n_elements(self):
-        self.assertEqual(numbers.get_amount_numbers("2,5,10,3,311"), [5])
+    def test_info_numbers_n_elements(self):
+        self.assertEqual(numbers.get_info_numbers("2,5,10,3,311"), [5])
 
-    def test_min_number_empty(self):
-        self.assertEqual(numbers.get_min_number(""), [0, 0])
 
 if __name__ == '__main__':
     unittest.main()
