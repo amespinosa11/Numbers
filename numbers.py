678fb472b2d2f565be9e6085baf8cd9b4d15c19f
Ana María Espinosa <am.espinosa11@uniandes.edu.co>
2018-09-29 21:36:24 -0500

    Paso5:Corregir get_info_numbers para que devuelva el maximo con 0,1+ elementos(verde)

--- numbers.py
+++ numbers.py
@@ -2,16 +2,16 @@ def get_info_numbers(numbers):
 	if numbers is "":
 		amount_numbers = 0
 		min_number = 0
+		max_number = 0
 
 	else:
 		num = numbers.split(",")
 		numbers_as_list = list(num)
 		amount_numbers = len(numbers_as_list)
 		min_number = int(min((int(i) for i in numbers_as_list)))
+		max_number = int(max((int(i) for i in numbers_as_list)))
 
-	result = [amount_numbers,min_number]
-	print(result)
+	result = [amount_numbers,min_number,max_number]
 	return result
 
-get_info_numbers("2,5")
 
