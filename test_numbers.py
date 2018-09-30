0fe8288c3ae098095b3e1238dd2d5757065706fc
ALEJANDRA SABOGAL <ja.sabogal@uniandes.edu.co>
2018-09-29 20:57:19 -0500

    Paso 2: Se crea el test cuando la cantidad de elementos es mayor a 2 (Rojo)

--- test_numbers.py
+++ test_numbers.py
@@ -1,9 +1,13 @@
-import unittest 
+import unittest
 import numbers
+
 
 class TestNumbers(unittest.TestCase):
     def test_amountNumbers_empty(self):
         self.assertEqual(numbers.get_amount_numbers(""), [0])
+
+    def test_amountNumbers_one_element(self):
+        self.assertEqual(numbers.get_amount_numbers("2"), [1])
 
 
 if __name__ == '__main__':
